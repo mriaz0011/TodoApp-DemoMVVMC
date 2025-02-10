70fbb7ab1d0fb916daf743caf4d54e6b1fbffa83
Muhammad Riaz <mriaz0011@yahoo.com>
2025-02-10 11:12:43 +0000

    Initial Commit

---
+++ PracticApp/PracticAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticAppApp.swift
+//  PracticApp
+//
+//  Created by Muhammad Riaz on 10/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct PracticAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PracticAppTests/PracticAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  PracticAppTests.swift
+//  PracticAppTests
+//
+//  Created by Muhammad Riaz on 10/02/2025.
+//
+
+import Testing
+@testable import PracticApp
+
+struct PracticAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
